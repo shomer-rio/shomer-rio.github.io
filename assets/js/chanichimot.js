51188159322579fa8c6b309dda1f283f9c019d18
Pedro <pedromxavier@poli.ufrj.br>
2020-07-02 02:11:39 -0300

    <3

--- assets/js/chanichimot.js
+++ assets/js/chanichimot.js
@@ -1,5 +1,33 @@
-function chanichimot(){
-    let element = document.getElementById('chanichimot');
-    element.innerHTML = '<p>CHANICHIM*OT</p>';
-    return;
+var div_element = document.getElementById('chanichimot');
+var width = div_element.offsetWidth;
+var height = div_element.offsetHeight;
+
+function setup(){
+    createCanvas(width, height);
 }
+
+function draw() {
+    translate(width / 2, height / 2);
+
+    /* Verifica se o mouse está dentro da área de desenho */
+    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+      let mx = mouseX - width / 2;
+      let my = mouseY - height / 2;
+      let pmx = pmouseX - width / 2;
+      let pmy = pmouseY - height / 2;
+      
+      /* Verifica se o mouse está pressionado */
+      if (mouseIsPressed) {
+        for (let i = 0; i < symmetry; i++) {
+          rotate(angle);
+          let sw = sizeSlider.value();
+          strokeWeight(sw);
+          line(mx, my, pmx, pmy);
+          push();
+          scale(1, -1);
+          line(mx, my, pmx, pmy);
+          pop();
+        }
+      }
+    }
+  }
