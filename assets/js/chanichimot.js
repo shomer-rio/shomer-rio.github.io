a203f7724e5b6b97e7fb3372293623ea31033eeb
Pedro <pedromxavier@poli.ufrj.br>
2020-07-02 05:10:19 -0300

    <3

--- assets/js/chanichimot.js
+++ assets/js/chanichimot.js
@@ -1,3 +1,6 @@
+var SHOW_CLUE = true;
+var step = 0;
+
 let symmetry = 6;
 let angle = 360 / symmetry;
 
@@ -13,13 +16,22 @@ function setup() {
     angleMode(DEGREES);
 }
 
+function inside(){
+    return (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height);
+}
+
 function draw() {
     translate(width / 2, height / 2);
 
+    if (SHOW_CLUE && !(frameCount % 4)){
+        magen_david(step++, 20);
+        return;
+    }
+    
     stroke(128);
 
     /* Verifica se o mouse está dentro da área de desenho */
-    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    if (inside()) {
         let mx = mouseX - width / 2;
         let my = mouseY - height / 2;
         let pmx = pmouseX - width / 2;
@@ -31,6 +43,7 @@ function draw() {
             for (let i = 0; i < symmetry; i++) {
                 rotate(angle);
                 strokeWeight(stroke_weight);
+                stroke(100);
                 line(mx, my, pmx, pmy);
                 push();
                 scale(1, -1);
@@ -41,6 +54,32 @@ function draw() {
     }
 }
 
+function mousePressed() {
+    if (SHOW_CLUE && inside()){
+        background(255);
+        SHOW_CLUE = false;
+    }
+}
+
 function windowResized() {
     resizeCanvas(width, height);
+}
+
+// Dica de como desenhar a Magen David
+function magen_david(step=0, total=10, offset=30) {
+    step = step % (total + offset)
+    if (step > total) return;
+
+    if (!step) {
+        background(255);
+    }
+
+    let dx = (height / 4) * Math.sqrt(3);
+    let y = (height / 4);
+
+    strokeWeight(5);
+    stroke(0);
+   
+    let x = lerp(-dx, 0, step/total);
+    point(x, y);
 }
