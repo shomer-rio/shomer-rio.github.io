6d682c5b0583a4d9b50b25e1b2463a646393daf9
Pedro <pedromxavier@poli.ufrj.br>
2020-08-20 01:45:12 -0300

    <3

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,4 +1,43 @@
-function buildTimeline(filePath){
+$.fn.timeline = function () {
+	var selectors = {
+		id: $(this),
+		item: $(this).find(".event"),
+		activeClass: "event--active",
+		img: ".timeline img"
+	};
+	
+	//
+	selectors.item.eq(0).addClass(selectors.activeClass);
+	
+	// Adds background image as the first one
+	selectors.id.css("background-image", `url(${selectors.item.first().find(selectors.img).attr("src")})`);
+
+	// Get item lenght
+	var itemLength = selectors.item.length;
+
+	// Triggered by srolling
+	$(window).scroll(function() {
+		var max, min;
+		var pos = $(this).scrollTop(); // Scroll position
+
+		selectors.item.each(function(i) {
+			min = $(this).offset().top;
+			max = $(this).height() + $(this).offset().top;
+		  
+			if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
+				selectors.item.removeClass(selectors.activeClass);
+				selectors.id.css("background-image",`url(${selectors.item.last().find(selectors.img).attr("src")})`);
+				selectors.item.last().addClass(selectors.activeClass);
+		  	} else if (pos <= max - 40 && pos >= min) {
+				selectors.id.css("background-image", `url(${$(this).find(selectors.img).attr("src")})`);
+				selectors.item.removeClass(selectors.activeClass);
+				$(this).addClass(selectors.activeClass);
+		  	}
+		});
+	});
+}
+
+function buildTimeline(filePath) {
     $.getJSON(filePath, function (array) {
 
 		let events = ""
@@ -16,67 +55,10 @@ function buildTimeline(filePath){
 			`
 		}
 
-		$("#timeline").html(`
-			<div class="timeline">
-				${events}
-			</div>
+		$(".timeline").html(`
+			${events}
 		`);
+
+		$("#timeline").timeline();
     });
 }
-
-(function($) {
-  $.fn.timeline = function() {
-    var selectors = {
-      id: $(this),
-      item: $(this).find(".event"),
-      activeClass: "event--active",
-      img: ".timeline img"
-    };
-    selectors.item.eq(0).addClass(selectors.activeClass);
-    selectors.id.css(
-      "background-image",
-      "url(" +
-        selectors.item
-          .first()
-          .find(selectors.img)
-          .attr("src") +
-        ")"
-    );
-    var itemLength = selectors.item.length;
-    $(window).scroll(function() {
-      var max, min;
-      var pos = $(this).scrollTop();
-      selectors.item.each(function(i) {
-        min = $(this).offset().top;
-        max = $(this).height() + $(this).offset().top;
-        var that = $(this);
-        if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
-          selectors.item.removeClass(selectors.activeClass);
-          selectors.id.css(
-            "background-image",
-            "url(" +
-              selectors.item
-                .last()
-                .find(selectors.img)
-                .attr("src") +
-              ")"
-          );
-          selectors.item.last().addClass(selectors.activeClass);
-        } else if (pos <= max - 40 && pos >= min) {
-          selectors.id.css(
-            "background-image",
-            "url(" +
-              $(this)
-                .find(selectors.img)
-                .attr("src") +
-              ")"
-          );
-          selectors.item.removeClass(selectors.activeClass);
-          $(this).addClass(selectors.activeClass);
-        }
-      });
-    });
-  };
-})(jQuery);
-
-$("#timeline").timeline();
