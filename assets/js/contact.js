30d2cb7d5ca3057376d9343e43048257ee18a463
Pedro <pedromxavier@poli.ufrj.br>
2020-09-17 15:18:48 -0300

    Update style

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,9 +3,18 @@ function buildContact( filePath ) {
         $('#contact').html(`
             <h1>${json['title']}</h1>
             <h2>${json['subtitle']}</h2>
-            <i class="fa fa-id-card"><span>${json['register']}</span></i>
-            <a class="fa fa-envelope" href="mailto:${json['email']}"><span>${json['email']}</span></a>
-            <a class="fa fa-phone" href="tel:${json['phone']}"><span>${json['phone']}</span></a>
+            <a>
+                <i class="fa fa-id-card"></i>
+                <span>${json['register']}</span>
+            </a>
+            <a href="mailto:${json['email']}">
+                <i class="fa fa-envelope"></i>
+                <span>${json['email']}</span>
+            </a>
+            <a href="tel:${json['phone']}">
+                <i class="fa fa-phone"></i>
+                <span>${json['phone']}</span>
+            </a>
         `);
     });
 }
