4de5e6f7538a378bd03824ef0ed5bb8ea2bed172
Pedro <pedromxavier@poli.ufrj.br>
2020-09-17 15:24:31 -0300

    Update width

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,15 +4,15 @@ function buildContact( filePath ) {
             <h1>${json['title']}</h1>
             <h2>${json['subtitle']}</h2>
             <a>
-                <i class="fa fa-id-card"></i>
+                <i class="fa fa-id-card fa-fw"></i>
                 <span>${json['register']}</span>
             </a>
             <a href="mailto:${json['email']}">
-                <i class="fa fa-envelope"></i>
+                <i class="fa fa-envelope fa-fw"></i>
                 <span>${json['email']}</span>
             </a>
             <a href="tel:${json['phone']}">
-                <i class="fa fa-phone"></i>
+                <i class="fa fa-phone fa-fw"></i>
                 <span>${json['phone']}</span>
             </a>
         `);
