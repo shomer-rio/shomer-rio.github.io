f7a39530c71ec0e20e3e9c33ff3f617089482581
Pedro <pedromxavier@poli.ufrj.br>
2020-09-17 14:29:30 -0300

    Fix style issued

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,9 +3,9 @@ function buildContact( filePath ) {
         $('#contact').html(`
             <h1>${json['title']}</h1>
             <h2>${json['subtitle']}</h2>
-            <p class="fa fa-address-card">${json['register']}</p>
-            <a class="fa fa-envelope" href="mailto:${json['email']}">${json['email']}</a>
-            <a class="fa fa-phone" href="tel:${json['phone']}">${json['phone']}</a>
+            <i class="fa fa-id-card"><span>${json['register']}</span></i>
+            <a class="fa fa-envelope" href="mailto:${json['email']}"><span>${json['email']}</span></a>
+            <a class="fa fa-phone" href="tel:${json['phone']}"><span>${json['phone']}</span></a>
         `);
     });
 }
