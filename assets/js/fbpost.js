7810a0224855283a56b87ff617cf1e15fdc99ffa
Pedro <pedromxavier@poli.ufrj.br>
2020-09-17 16:11:43 -0300

    Add https

--- assets/js/fbpost.js
+++ assets/js/fbpost.js
@@ -2,7 +2,7 @@ function buildPost( pageId ) {
     $('#fbpost').html(`
     <iframe
         scrolling="yes"
-        src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2F${pageId}&width=500&height=600&stream=true&header=true"
+        src="https://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2F${pageId}&width=500&height=600&stream=true&header=true"
     >
     </iframe>
     `)
